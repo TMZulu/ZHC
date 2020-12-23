f3475abe8d1d42ba6413f5fa445754648e82fa40
TMZulu <tmzuluyt@gmail.com>
2020-12-22 22:46:19 -0600

    created extras and stat

---
+++ addons/stat/config.cpp
@@ -0,0 +1,16 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"zhc_main"};
+        author = ECSTRING(main,Author);
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgEventHandlers.hpp"
---
+++ addons/stat/fnc_empty.sqf
@@ -0,0 +1,16 @@
+#include "script_component.hpp"
+/*
+ * Author: NAME
+ * Short Description
+ *
+ * Arguments:
+ * 0: NONE
+ *
+ * Return Value:
+ * NONE
+ *
+ * Example:
+ * [] call zhc_blank_fnc_empty
+ *
+ * Public: No
+ */
